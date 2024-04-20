e11d7f830a51625b31d6749c793ffff0cb7bd20b
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-04-20 00:16:51 +0000

    initial_commit

---
+++ howdy.rb
@@ -0,0 +1,15 @@
+require "active_support/all"
+# require "./goodbye.rb"
+opener_string = "Hello, world!"
+# pp opener_string
+
+# pp 1.ordinalize
+# pp 2.ordinalize
+# pp 3.ordinalize
+# pp 4.ordinalize
+
+puts "What's your name?"
+
+their_name = gets.chomp
+
+puts "Hi there, " + their_name + "!"
